9a8ad75f04ba727744e65fd451b3221b9ccae3c8
Reiniel Fernandez <reinielfc@gmail.com>
2023-04-16 11:32:44 -0400

    update with DupeGroup class

--- hiccup_remedy.py
+++ hiccup_remedy.py
@@ -1,80 +1,68 @@
-import argparse
-import json
 import os
-import pathlib
-import random
 import re
-import string
-import tempfile
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 
-import sh
+from sh import Command
+
+from dupe_group import DupeGroup
 
 
 def main():
     args = get_args()
-    config = {'delete_method': 'NONE', 'dryrun': True}
 
-    dupes = get_dupes(args.dirs, config, args.key_length)
+    print(args)
+    dupes = find_dupes(args.dirs, args.dest, dict())
 
-    json_string = json.dumps(dupes)
-    print(json_string)
+    [print(d) for d in dupes]
 
 
-def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        prog='hiccup_remedy', description='')
+def get_args() -> Namespace:
+    parser = ArgumentParser()
 
     parser.add_argument(
-        '-k', '--key-length', type=int, default=16,
-        help='length of directory names for each group')
-
-    parser.add_argument(
-        '-P', '--preserve-path', action='store_true',
-        help='file is renamed as its path separated by periods')
+        '-d', '--dest', type=Path, default=Path(os.getcwd()),
+        help='destination of dupes')
 
     parser.add_argument(
         '-n', '--dry-run', action='store_true',
         help='do not change files')
 
     parser.add_argument(
-        dest='dirs', metavar='DIR', nargs='+', type=pathlib.Path,
-        help='directory paths to find duplicates at', )
+        dest='dirs', metavar='DIR', nargs='+', type=Path,
+        help='directory paths to find duplicates at')
 
     return parser.parse_args()
 
 
-def get_dupes(dirs: list, config: dict, key_length: int = 16) -> dict[dict]:
-    cmd = sh.Command('czkawka-cli')
+def find_dupes(dirs: list[Path], dest: Path, config: dict):
+    czkawka = Command('czkawka-cli')
 
-    with tempfile.NamedTemporaryFile() as tmp:
-        config['file_to_save'] = pathlib.Path(tmp.name).absolute()
+    with NamedTemporaryFile() as tmp:
+        config.update(  # set default config
+            dryrun=True,
+            file_to_save=Path(tmp.name).resolve())
 
-        cmd('dup', **config,
-            d=[dir_path.absolute() for dir_path in dirs],
+        czkawka(  # run command
+            'dup', **config,
+            d=[d.resolve() for d in dirs],
             _out=os.devnull, _err=os.devnull)
 
-        dupes = {
-            generate_key(key_length): parse_group(tmp, line)
-            for line in tmp
-            if line.startswith(b'---- Size')}
+        headers = filter(lambda line: line.startswith(b'---- Size'), tmp)
+        groups = map(lambda header: parse_group(tmp, header), headers)
 
-    return dupes
+        return list(groups)
 
 
-def generate_key(length: int):
-    alphanumeric = string.ascii_letters + string.digits
-    return ''.join(random.choice(alphanumeric) for i in range(length))
+def parse_group(tmp_file, line: bytes) -> DupeGroup:
+    match = re.search(br'Size [\d.]+ [KMGT]iB \((\d+)\) - (\d+) files?', line)
+    size, num_files = map(int, match.groups())
+    records = [
+        Path(tmp_file.readline().decode('ascii').rstrip('\n'))
+        for i in range(num_files)]
 
-
-def parse_group(tmp, line) -> dict:
-    match = re.search(rb'Size [\d.]+ [KMGT]iB \((\d+)\) - (\d+) files?', line)
-    size, count = [int(group) for group in match.groups()]
-
-    return {
-        'size': size,
-        'matches': [
-            tmp.readline().decode('ascii').rstrip('\n')
-            for i in range(count)]}
+    return DupeGroup(size, records)
 
 
 if __name__ == '__main__':
