a4dd9a4cc3cbb2f5f6d3537eeccf971bcb12b869
Reiniel Fernandez <reinielfc@gmail.com>
2023-04-16 19:33:52 -0400

    remove FileGroup implementation

--- hiccup/filegroup.py
+++ hiccup/filegroup.py
@@ -9,38 +9,21 @@ from util import human_readable
 
 
 class FileGroup:
-    def __init__(self, record_list: list[str], **attr: dict):
-        self._key: UUID = uuid4()
-        self._attr: dict = attr
-        self._records: list = [self._make_record(r) for r in record_list]
-        self._common = Path(os.path.commonpath(
-            [r['orig']['path'] for r in self._records]))
-        self._set_record_names()
+    def __init__(self, **attr):
+        self._id = uuid4()
+        self._attr = attr
+        self._files: list[dict] = []
 
-    def _make_record(self, record_str: str) -> dict:
-        pass
-
-    def _set_record_names(self):
-        for r in self._records:
-            r['name'] = str(r['orig']['path'].relative_to(self._common)).replace('/', '.')
 
     def move(self, dest: Path):
-        key_path = Path(str(self._key))
-        dest_dir = dest.joinpath(key_path)
-        dest_dir.mkdir(exist_ok=True)
-
-        for r in self._records:
-            orig: Path = r['orig']['path']
-            dest: Path = dest_dir.joinpath(r['name'])
-
-            orig.rename(dest)
+        pass
 
     @staticmethod
     def from_header(header: bytes, open_file: io.BufferedIOBase) -> 'FileGroup':
         pass
 
     @staticmethod
-    def str_is_header(line: str | bytes) -> bool:
+    def is_header(line: str | bytes) -> bool:
         pass
 
 
