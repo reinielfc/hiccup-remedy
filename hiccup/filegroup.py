ad09bbc154d10ee06078d1c632a9c94404215292
Reiniel Fernandez <reinielfc@gmail.com>
2023-04-16 16:26:07 -0400

    implement DuplicateFileGroup

--- hiccup/filegroup.py
+++ hiccup/filegroup.py
@@ -42,3 +42,29 @@ class FileGroup:
     @staticmethod
     def str_is_header(line: str | bytes) -> bool:
         pass
+
+
+class DuplicateFileGroup(FileGroup):
+    def __str__(self):
+        size = self._attr['size']
+        lines = [f'---- {self._key} - Size {human_readable(size)} ({size}) - {len(self._records)} files']
+        lines += [str(r['orig']['path']) for r in self._records]
+
+        return '\n'.join(lines) + '\n'
+
+    def _make_record(self, record_str: str) -> dict:
+        return {'orig': dict(path=Path(record_str)), 'dest': dict(path=None)}
+
+    @staticmethod
+    def from_header(header: bytes, open_file: io.BufferedIOBase) -> FileGroup:
+        match = re.search(br'Size [\d.]+ [KMGT]iB \((\d+)\) - (\d+) files?', header)
+        size, num_files = map(int, match.groups())
+        record_list = [
+            open_file.readline().decode('ascii').rstrip('\n')
+            for i in range(num_files)]
+
+        return DuplicateFileGroup(record_list, size=size)
+
+    @staticmethod
+    def str_is_header(line: str | bytes) -> bool:
+        return line.startswith(b'---- Size')
